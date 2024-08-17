df01e398bbd256f9fb72c1ffefc0841944c9ce55
MatheusEmanoel25 <matheusemanoeldsp@gmail.com>
2024-08-16 23:38:42 -0300

    add informação

--- script.js
+++ script.js
@@ -6,38 +6,45 @@ const usersDB = {
 
 //  login
 document.getElementById('loginForm')?.addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    const username = document.getElementById('username').value;
+    event.preventDefault(); //impede o envio do formularo que carregaria a pagina
+   
+   // pegar os dados de entrada do usuário
+    const username = document.getElementById('username').value; 
     const password = document.getElementById('password').value;
     
     if (usersDB[username] === password) {
-        alert('Login bem-sucedido!');
+        alert('Login bem-sucedido!'); //verificando as informações existe no BD
        
         location.href = 'index.html'; 
-        document.getElementById('errorMessage').innerText = 'Usuário ou senha incorretos!';
-        document.getElementById('errorMessage').classList.remove('hidden');
+        document.getElementById('errorMessage').innerText = 'Usuário ou senha incorretos!'; //se as informação estive correta sera direcionado para index
+        document.getElementById('errorMessage').classList.remove('hidden'); //se o login falhou aparece mensagem de erro na tela
     }
 });
 
 //  cadastro
+
+// seleciona o formulario de cadstro
 document.getElementById('cadastroForm')?.addEventListener('submit', function(event) {
-    event.preventDefault();
+    event.preventDefault(); 
     
+    //pegar os dados do novo  usuário
     const newUsername = document.getElementById('newUsername').value;
     const newPassword = document.getElementById('newPassword').value;
     
+    //verifica o BD, se tive dados existente messagem de erro na tela  
     if (usersDB[newUsername]) {
         document.getElementById('registerMessagem').innerText = 'Usuário já existe!';
         document.getElementById('registerMessagem').classList.remove('hidden');
     } else {
         usersDB[newUsername] = newPassword;
         alert('Cadastro bem-sucedido! Faça login.');
-        location.href = 'login.html'; 
+        location.href = 'login.html'; //adicionar  informações no BD volta para tela de login
     }
 });
 // home 
 
+//se o usuário fazer o login sera encaminhado para o site
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault(); 
   window.location.href = 'home.html'; }
+
